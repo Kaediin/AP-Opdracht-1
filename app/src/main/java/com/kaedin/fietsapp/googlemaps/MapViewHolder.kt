56387df70cdffe6655062fab4237bd6eb35dbbe6
kaediin <skaedin@gmail.com>
2021-04-23 09:29:29 +0200

    Added argumentation (kDocs) to:  - MapViewHolder.kt  - MapViewListItemView.kt

--- app/src/main/java/com/kaedin/fietsapp/googlemaps/MapViewHolder.kt
+++ app/src/main/java/com/kaedin/fietsapp/googlemaps/MapViewHolder.kt
@@ -1,4 +1,4 @@
-package com.kaedin.spacex.googlemaps
+package com.kaedin.fietsapp.googlemaps
 
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
@@ -8,22 +8,30 @@ class MapViewHolder(private val mMapViewListItemView: MapViewListItemView?) :
     RecyclerView.ViewHolder(
         mMapViewListItemView!!
     ) {
+    /**
+     * Recyclerviewholder for the MapsView. This class is used to link a GoogleMaps widget with a list item from a recyclerview
+     * @see RecyclerView.ViewHolder
+     */
     fun mapViewListItemViewOnCreate(savedInstanceState: Bundle?) {
         mMapViewListItemView?.mapViewOnCreate(savedInstanceState)
     }
 
+    /** Pass vehicle to the listItemView */
     fun mapViewListItemViewOnResume(vehicle: Vehicle) {
         mMapViewListItemView?.mapViewOnResume(vehicle)
     }
 
+    /** Call the pause method */
     fun mapViewListItemViewOnPause() {
         mMapViewListItemView?.mapViewOnPause()
     }
 
+    /** Call the destroy method */
     fun mapViewListItemViewOnDestroy() {
         mMapViewListItemView?.mapViewOnDestroy()
     }
 
+    /** Call the low memory method */
     fun mapViewListItemViewOnLowMemory() {
         mMapViewListItemView?.mapViewOnLowMemory()
     }
--- app/src/main/java/com/kaedin/fietsapp/googlemaps/MapViewListItemView.kt
+++ app/src/main/java/com/kaedin/fietsapp/googlemaps/MapViewListItemView.kt
@@ -1,4 +1,4 @@
-package com.kaedin.spacex.googlemaps
+package com.kaedin.fietsapp.googlemaps
 
 import android.content.Context
 import android.content.Intent
@@ -21,16 +21,31 @@ import kotlinx.android.synthetic.main.list_item.view.*
 
 class MapViewListItemView constructor(context: Context?, attrs: AttributeSet? = null) :
     CardView(context!!, attrs), OnMapReadyCallback {
+
+    /**
+     * Global scoped vars
+     */
     private var mMapView: MapView? = null
     private var mVehicle: Vehicle? = null
     private var mView: View? = null
 
+    /**
+     * Inflate view with the list item layout
+     * This view is now the context in which you can alter the text values
+     * Set the mapview to the mapview from the context from the inflated view
+     * @see LayoutInflater.inflate
+     * @see MapView
+     */
     private fun setupView() {
         mView = LayoutInflater.from(context).inflate(R.layout.list_item, this)
         mView!!.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary))
         mMapView = mView!!.findViewById<View>(R.id.vehicle_list_item_map) as MapView
     }
 
+    /** create map is activity is create
+     * We dont handle any logic here we want to first create and then when resumed (which always follows next)
+     * we can handle logic because we know for sure the mapView instance is created and wont cause a NullpointerException
+     */
     fun mapViewOnCreate(savedInstanceState: Bundle?) {
         if (mMapView != null) {
             mMapView!!.onCreate(savedInstanceState)
@@ -40,6 +55,7 @@ class MapViewListItemView constructor(context: Context?, attrs: AttributeSet? = 
     fun mapViewOnResume(vehicle: Vehicle) {
         if (mMapView != null) {
             mMapView!!.onResume()
+            /**  */
             mVehicle = vehicle
 
             mView?.vehicle_id?.text = vehicle.type.toString()
@@ -53,18 +69,21 @@ class MapViewListItemView constructor(context: Context?, attrs: AttributeSet? = 
         }
     }
 
+    /** Pasue map is activity is paused */
     fun mapViewOnPause() {
         if (mMapView != null) {
             mMapView!!.onPause()
         }
     }
 
+    /** Destroy map is activity is destroyed */
     fun mapViewOnDestroy() {
         if (mMapView != null) {
             mMapView!!.onDestroy()
         }
     }
 
+    /** Enable low memory if needed */
     fun mapViewOnLowMemory() {
         if (mMapView != null) {
             mMapView!!.onLowMemory()
